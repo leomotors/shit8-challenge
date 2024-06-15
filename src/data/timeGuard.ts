3775364bfcabca8b65cc3f424b1393b2403f1d8a
Nutthapat Pongtanyavichai <59821765+leomotors@users.noreply.github.com>
2024-06-16 01:26:13 +0700

    ready

--- src/data/timeGuard.ts
+++ src/data/timeGuard.ts
@@ -16,4 +16,4 @@ function createGuard(timeStr: string) {
 // export const quizTimeGuard = createGuard("2024-06-13T13:00:00Z");
 // export const raputaTimeGuard = createGuard("2024-06-13T13:30:00Z");
 
-export const mcTimeGuard = createGuard("2024-06-16T13:00:00Z");
+export const finalBatchGuard = createGuard("2024-06-16T13:00:00Z");
