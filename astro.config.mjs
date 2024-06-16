6271e6317260383100a99b6d5c510274f474a38b
Nutthapat Pongtanyavichai <59821765+leomotors@users.noreply.github.com>
2024-06-17 01:45:54 +0700

    feat: build for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
